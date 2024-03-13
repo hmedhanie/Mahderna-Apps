3563e29dfe4cb0da9733b5762c059ad6048ad6d0
Medhanie Gebreigzabher <medhaniehabtu9@gmail.com>
2024-03-13 12:51:28 -0500

    first Project setup

---
+++ src/main/java/com/example/mahdernaapps/MahdernaAppsApplication.java
@@ -0,0 +1,13 @@
+package com.example.mahdernaapps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MahdernaAppsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MahdernaAppsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mahdernaapps/MahdernaAppsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mahdernaapps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MahdernaAppsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
